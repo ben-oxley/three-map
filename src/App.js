f2c94114d377d5d0b15ff2a79103462774f87e16
Ben Oxley <ben.oxley@gmail.com>
2023-11-28 21:30:58 +0000

    Animated lighting

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
     });
     return g;
   }, [geom]);
+  
   return (
     <div id="canvas-container">
       <Canvas shadows={{ type: "BasicShadowMap" }}>
@@ -33,8 +34,9 @@ function App() {
         <mesh ref={ref} position={[0, 0, 371.62 / 2]} rotation={[0, 0, 0]} geometry={geometry} castShadow receiveShadow>
           <meshStandardMaterial map={colorMap} />
         </mesh>
-        <pointLight castShadow position={[300, 100, 0]} intensity={100000} color="#fff" shadow-mapSize-height={512}
-          shadow-mapSize-width={512} shadow-camera-far={1000} shadow-camera-near={1} />
+        {/* <pointLight castShadow position={[Math.sin(count.current), 100, Math.cos(count.current)]} intensity={100000} color="#fff" shadow-mapSize-height={512}
+          shadow-mapSize-width={512} shadow-camera-far={1000} shadow-camera-near={1} /> */}
+        <PointLight></PointLight>
         <Plane
           receiveShadow
           rotation={[-Math.PI / 2, 0, 0]}
@@ -56,6 +58,21 @@ function Controls(props) {
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <CameraControls ref={cameraControlsRef} azimuthAngle={3.14159265 / 2} polarAngle={0} makeDefault />
+  )
+}
+
+function PointLight(props){
+  const count = useRef(0.0);
+  const light = useRef();
+  useFrame(() => {
+    
+    count.current = count.current + 0.01;
+    light.current.position.x = Math.cos(count.current)*300;
+    light.current.position.z = Math.sin(count.current)*300;
+  })
+  return (
+    <pointLight castShadow position={[300,100,0]} intensity={100000} color="#fff" shadow-mapSize-height={512}
+    shadow-mapSize-width={512} shadow-camera-far={1000} shadow-camera-near={1} ref={light} />
   )
 }
 
